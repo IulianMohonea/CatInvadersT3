6a3045c8bf59717dba7c5fcde0109d2f8f09c3ce
IulianMohonea <126501200+IulianMohonea@users.noreply.github.com>
2023-06-25 22:59:25 +0300

    Add files via upload

--- Errors.cpp
+++ Errors.cpp
@@ -1,6 +1,6 @@
 #include "Errors.h"
 playerErrors::playerErrors(std::string& mesaj) :
-Errors( std::string ("Miscare invalida: ") + mesaj + '\n') {}
+Errors(mesaj) {}
 
 fireErrors::fireErrors(std::string& mesaj) :
-Errors(std::string("Timp prea putin intre gloante: ") + mesaj + '\n') {}
+Errors(mesaj) {}
